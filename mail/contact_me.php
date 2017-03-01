47cb8c2fea08bb239acb39054d26dbcfb5b32de4
Francisco Espinosa <francisco.espinosa@fandeal.com.mx>
2017-03-01 10:18:06 -0600

    Deleted non used images and change mail on contactme script

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,11 +16,11 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'paco-rock.com.mx@hotmail.com'; //Hay que cambiar mi mail por una dirección de blusoft, hay que ver si
+$to = 'paco-rock.com.mx@hotmail.com, francisco.espinosa@blusofthouse.com.mx'; //Hay que cambiar mi mail por una dirección de blusoft, hay que ver si
 //cuando tengamos la página armamos una con ese dominio, o usamos una de gmail o algo.
 $email_subject = "Un cliente potencial quiere contactarnos:  $name";
 $email_body = "Datos de contacto (Potenciales clientes Blu-Soft House).\n\n"."Detalles:\n\nNombre: $name\n\nEmail: $email_address\n\nTelefono: $phone\n\nMensaje:\n$message";
-$headers = "From: noreply@blusofthouse.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@blusofthouse.com.mx\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
